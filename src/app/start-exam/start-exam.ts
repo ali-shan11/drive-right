66e22ee7f507d519e98462ea2f94e7a51422c69b
Ali Shan <ali.shan.khalid01@gmail.com>
2026-05-18 15:43:05 +0500

    exam ui change

--- src/app/start-exam/start-exam.ts
+++ src/app/start-exam/start-exam.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Rules } from "./rules/rules";
 
 @Component({
   selector: 'app-start-exam',
-  imports: [],
+  imports: [Rules],
   templateUrl: './start-exam.html',
   styleUrl: './start-exam.scss',
 })
 export class StartExam {
+  public showRules = true;
 }
